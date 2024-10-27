88569d7a0c1aa95f02f58c009b2de4385a05f03f
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2024-10-28 02:24:25 +1300

    Interface for classes that require to be updated after a record change

---
+++ src/main/java/Screens/Updateable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Screens;
+
+/**
+ *
+ * @author nicol
+ * 
+ * For panels that require to be updated after changes to the storage
+ */
+public interface Updateable {
+    public void update();
+}
