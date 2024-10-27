f089570e6917837c050346f72ab7291b54c313e4
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2024-10-28 02:04:58 +1300

    Added new 'generic' methods for handling requests based on a FinancialRecordType parameter

--- src/main/java/Expenses/BudgetManager.java
+++ src/main/java/Expenses/BudgetManager.java
@@ -14,7 +14,7 @@ import Enums.FinancialRecordType;
  * ExpenseManager class to manage all expenses
  */
 public final class BudgetManager {
-    
+
     private final Storage expenses;  // Stores the expenses
     private final Storage incomes; // Stores the incomes
 
@@ -24,12 +24,12 @@ public final class BudgetManager {
         this.incomes = new DBStorage(new DatabaseManager(), FinancialRecordType.INCOME);
     }
 
-    // Public methods
+    // retrieves budget
     public double getBudget() {
         return calculateTotalIncomes() - calculateTotalExpenses();
     }
 
-    // handles the storage of a financial record with the FinancialRecordType
+    // sets a single record via the type
     public void addRecord(FinancialRecord fr, FinancialRecordType type) {
         if (type == FinancialRecordType.EXPENSE) {
             addExpense(fr);
@@ -38,67 +38,126 @@ public final class BudgetManager {
             addIncome(fr);
         }
     }
-    
-    // internal usage
+
+    // retrieves a single record configured via the type
+    public FinancialRecord getRecord(String name, FinancialRecordType type) {
+        if (type == FinancialRecordType.EXPENSE) {
+            return getExpense(name);
+        }
+        if (type == FinancialRecordType.INCOME) {
+            return getIncome(name);
+        }
+
+        // in case something goes wrong
+        return null;
+    }
+
+    // retrieves all records of a certain type
+    public FinancialRecord[] getRecords(FinancialRecordType type) {
+        if (type == FinancialRecordType.EXPENSE) {
+            return getExpenses();
+        }
+        if (type == FinancialRecordType.INCOME) {
+            return getIncomes();
+        }
+
+        // in case something goes wrong
+        return null;
+    }
+
+    // deletes a record by name based on type
+    public boolean deleteRecord(String name, FinancialRecordType type) {
+        if (type == FinancialRecordType.EXPENSE) {
+            return deleteExpense(name);
+        }
+        if (type == FinancialRecordType.INCOME) {
+            return deleteIncome(name);
+        }
+
+        // in case something goes wrong
+        return false;
+    }
+
+    // clears all records of a certain type
+    public void clearStorage(FinancialRecordType type) {
+        if (type == FinancialRecordType.EXPENSE) {
+            clearExpenses();
+        }
+        if (type == FinancialRecordType.INCOME) {
+            clearIncomes();
+        }
+    }
+
+    // retrieves the total amount of a certain record type
+    public double getTotal(FinancialRecordType type) {
+        if (type == FinancialRecordType.EXPENSE) {
+            return calculateTotalExpenses();
+        }
+        if (type == FinancialRecordType.INCOME) {
+            return calculateTotalIncomes();
+        }
+
+        return 0;
+    }
+
+    // internal set
     private void addIncome(FinancialRecord income) {
         this.incomes.set(income);
     }
-    
+
     private void addExpense(FinancialRecord expense) {
         expenses.set(expense);  // Adds expense to storage
     }
 
     // get methods
-    public FinancialRecord getExpense(String name) {
+    private FinancialRecord getExpense(String name) {
         return expenses.get(name);
     }
-    
-    public FinancialRecord getIncome(String name) {
+
+    private FinancialRecord getIncome(String name) {
         return incomes.get(name);
     }
 
-    // delete methods
-    public void deleteExpense(String name) {
-        if (!expenses.remove(name)) {
-            System.out.println("Couldn't find \"" + name + "\"");
-        }
+    // returns arrays
+    public FinancialRecord[] getExpenses() {
+        return expenses.getArray();
     }
-    
-    public void deleteIncome(String name) {
-        if (!incomes.remove(name)) {
-            System.out.println("Couldn't find \"" + name + "\"");
-        }
+
+    public FinancialRecord[] getIncomes() {
+        return incomes.getArray();
+    }
+
+    // delete methods
+    private boolean deleteExpense(String name) {
+        return expenses.remove(name);
+    }
+
+    private boolean deleteIncome(String name) {
+        return incomes.remove(name);
     }
 
     // calculate totals
-    public double calculateTotalExpenses() {
+    private double calculateTotalExpenses() {
         double totalExpenses = 0.0;
         for (FinancialRecord expense : expenses.getArray()) {  // Loop through expenses to calculate total
             totalExpenses += expense.getAmount();
         }
         return totalExpenses;
     }
-    
-    public double calculateTotalIncomes() {
+
+    private double calculateTotalIncomes() {
         double totalIncomes = 0.0;
         for (FinancialRecord income : incomes.getArray()) {  // Loop through expenses to calculate total
             totalIncomes += income.getAmount();
         }
         return totalIncomes;
     }
-    
-    public FinancialRecord[] getExpenses() {
-        return expenses.getArray();
-    }
-    
-    public FinancialRecord[] getIncomes() {
-        return incomes.getArray();
-    }
-    
+
+    // clears
     public void clearExpenses() {
         expenses.clear(); // Assuming you're using a List<Expense> called expenses
     }
-    
+
     public void clearIncomes() {
         incomes.clear();
     }
